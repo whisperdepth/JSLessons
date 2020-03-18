40c96d31cdfdf0d58ada8228ab00a78c5fdbe9c2
Roman <whisperdepth@gmail.com>
2020-03-18 16:33:48 +0200

    Add files via upload

---
+++ lesson2/1/index.js
@@ -0,0 +1,8 @@
+const userName = "John";
+const greeting = "I'm John";
+const _profession = "Developer";
+const $hobby = "Walking";
+const FavouriteDessert = "cake";
+let pet_ = "dog";
+let superNumber;
+superNumber = 17;
