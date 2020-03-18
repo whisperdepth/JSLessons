31e856b44326787241ebc8337c3ffa059b1d23fa
Roman <whisperdepth@gmail.com>
2020-03-18 18:08:07 +0200

    Update index.js

--- lesson2/task3/index.js
+++ lesson2/task3/index.js
@@ -5,6 +5,6 @@ console.log(goodNumber);
 console.log(anotherNumber);
 let customer = { name: "John", age: 17 };
 let anotherCustomer = customer;
-anotherNumber.age = 20;
+anotherCustomer.age = 20;
 console.log(customer.age);
 console.log(anotherCustomer.age);
