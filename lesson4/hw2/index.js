8e66f3f14c42830ca67d7f55f58cb50257af5a89
Roman <whisperdepth@gmail.com>
2020-03-19 21:26:42 +0200

    Update index.js

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -3,11 +3,23 @@ const n = 17;
 let result = 0;
 for (let i = m; i <= n; i++) {
   if (i % 2 === 0 && i % 4 != 0) {
-    result += i;
+    if (i % 5 === 0) {
+      console.log(i);
+    } else {
+      result += i;
+    }
   } else if (i % 3 === 0) {
-    result -= i;
+    if (i % 5 === 0) {
+      console.log(i);
+    } else {
+      result -= i;
+    }
   } else if (i % 4 === 0) {
-    result *= i;
+    if (i % 5 === 0) {
+      console.log(i);
+    } else {
+      result *= i;
+    }
   } else if (i % 5 === 0) {
     console.log(i);
   } else {
