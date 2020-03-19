4958c98f8febb140599b6c39a266125b96f65419
Roman <whisperdepth@gmail.com>
2020-03-19 13:34:52 +0200

    Add files via upload

---
+++ lesson3/task5/index.js
@@ -0,0 +1,33 @@
+if (true) console.log("I'm in");
+
+if (false) {
+  console.log("I'm not in");
+}
+
+if ("text") {
+  console.log("Yes");
+}
+
+if (null) {
+  console.log("Yes");
+} else {
+  console.log("Else");
+}
+
+let age = 20;
+
+if (age < 16) {
+  console.log("Too young");
+} else if (age >= 16 && age < 18) {
+  console.log("Almost ready");
+} else if (age >= 18 && age < 65) {
+  console.log("Ready");
+} else {
+  console.log("Else");
+}
+
+// Тернаный оператор ? : ( const result = условие ? значение1 : значение2; )
+
+let amount = 15;
+const result = amount > 17 ? "full" : "empty";
+console.log(result);
