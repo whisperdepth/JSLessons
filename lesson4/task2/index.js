43f2f06ed55a736844f41a2c9b799f26c882c74a
Roman <whisperdepth@gmail.com>
2020-03-19 19:24:33 +0200

    Update index.js

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,12 +1,10 @@
-let m = 3;
-let n = 9;
+const m = 3;
+const n = 9;
 let result = 1;
 
-do {
-  if (m % 2 === 1) {
-    result *= m;
+for (let i = m; i <= n; i++) {
+  if (i % 2 === 1) {
+    result *= i;
   }
-  m++;
-} while (m <= n);
-
+}
 console.log("Result: ", result);
