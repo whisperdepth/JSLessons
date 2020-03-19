a9bc1626bfec33b7e169c6e8e0b5b115c1b81d29
Roman <whisperdepth@gmail.com>
2020-03-19 23:06:55 +0200

    Update index.js

--- lesson4/hw3/index.js
+++ lesson4/hw3/index.js
@@ -1,12 +1,9 @@
-let sum = 0;
 for (let i = 0; i <= 1000; i++) {
-  if (i % 2 === 1) {
-    console.log("Found");
-    sum += i;
-  }
+  if (i % 2 === 0) continue;
+  console.log("Found");
+  sum += i;
 }
-let result = sum * 5;
-if (result > 5000) {
+if (sum * 5 > 5000) {
   console.log("Bigger");
 } else {
   console.log("Smaller or equal");
