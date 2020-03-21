0fe92e69b21beb96c43832a6fd01680fa6019db7
Roman <whisperdepth@gmail.com>
2020-03-22 01:59:17 +0200

    Update index.js

--- lesson6/hw8/index.js
+++ lesson6/hw8/index.js
@@ -1,4 +1,28 @@
-function sortAsc(array){}
+function sortAsc(array) {
+    let iterations = array.length;
+    let sorted = [];
+    let min = Infinity;
+    if (!Array.isArray(array)) {
+      return null;
+    }
+    for (let n = 0; n < iterations; n++) {
+       for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) {
+          min = array[i];
+        }
+      }
+      sorted.push(min);
+      min = Infinity;
+      here: for (let i = 0; i < array.length; i++) {
+        for (j = 0; j < sorted.length; j++) {
+          if (array[i] === sorted[j]) {
+            array.splice(i, 1); break here;
+          }
+        }
+      }
+    }
+    return sorted;
+  }
 
 function sortDesc(array) {
   let iterations = array.length;
