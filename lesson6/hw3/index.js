dfe322a6272a851d85302cdb604a6b8c1a54f164
Roman <whisperdepth@gmail.com>
2020-03-21 18:21:43 +0200

    Add files via upload

---
+++ lesson6/hw3/index.js
@@ -0,0 +1,14 @@
+function checkSum (arr){
+    if(!Array.isArray(arr){
+        return null;
+    })
+    let sum = 0;
+    for (let num of arr){
+        sum += num;
+    }
+    if(sum > 100){
+        return true;
+    }else{
+        return false;
+    }
+}
