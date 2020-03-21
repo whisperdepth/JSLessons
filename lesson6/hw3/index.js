af39fd52cb4c831983c2e9b1788bf25f07a49c8d
Roman <whisperdepth@gmail.com>
2020-03-21 18:25:37 +0200

    Update index.js

--- lesson6/hw3/index.js
+++ lesson6/hw3/index.js
@@ -1,14 +1,14 @@
-function checkSum (arr){
-    let sum = 0;
-    if(!Array.isArray(arr){
-        return null;
-    })
-    for (let num of arr){
-        sum += num;
-    }
-    if(sum > 100){
-        return true;
-    }else{
-        return false;
-    }
+function checkSum(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  if (sum > 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
