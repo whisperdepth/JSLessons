79ad0569eba058415992c69f1ea27669ea340c60
Roman <whisperdepth@gmail.com>
2020-03-22 01:26:15 +0200

    Add files via upload

---
+++ lesson6/hw8/index.js
@@ -0,0 +1,29 @@
+function sortAsc(array) {
+  let iterations = array.length;
+  let sorted = [];
+  let max = -Infinity;
+  if (!Array.isArray(array)) {
+    return null;
+  }
+  for (let n = 0; n < iterations; n++) {
+    for (let i = 0; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i];
+      }
+    }
+    sorted.push(max);
+    max = -Infinity;
+    for (let i = 0; i < array.length; i++) {
+      for (j = 0; j < sorted.length; j++) {
+        if (array[i] === sorted[j]) {
+          array.splice(i, 1);
+        }
+      }
+    }
+  }
+  return sorted;
+}
+
+let array1 = [1, 4, 4, 5, 9, 9, 2, 7, 0];
+console.log(sortAsc(array1));
+console.log(array1);
