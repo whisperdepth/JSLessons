e76116eae3543b46c2e895bdde6d8360d5f49a2c
Roman <whisperdepth@gmail.com>
2020-03-22 02:07:21 +0200

    Update index.js

--- lesson6/hw8/index.js
+++ lesson6/hw8/index.js
@@ -1,29 +1,3 @@
-function sortDesc(array) {
-    let iterations = array.length;
-    let sorted = [];
-    let min = Infinity;
-    if (!Array.isArray(array)) {
-      return null;
-    }
-    for (let n = 0; n < iterations; n++) {
-       for (let i = 0; i < array.length; i++) {
-        if (min > array[i]) {
-          min = array[i];
-        }
-      }
-      sorted.push(min);
-      min = Infinity;
-      here: for (let i = 0; i < array.length; i++) {
-        for (j = 0; j < sorted.length; j++) {
-          if (array[i] === sorted[j]) {
-            array.splice(i, 1); break here;
-          }
-        }
-      }
-    }
-    return sorted;
-  }
-
 function sortAsc(array) {
   let iterations = array.length;
   let sorted = [];
@@ -50,4 +24,32 @@ function sortAsc(array) {
   return sorted;
 }
 
+
+
+
+function sortDesc(array) {
+    let iterations = array.length;
+    let sorted = [];
+    let min = Infinity;
+    if (!Array.isArray(array)) {
+      return null;
+    }
+    for (let n = 0; n < iterations; n++) {
+       for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) {
+          min = array[i];
+        }
+      }
+      sorted.push(min);
+      min = Infinity;
+      here: for (let i = 0; i < array.length; i++) {
+        for (j = 0; j < sorted.length; j++) {
+          if (array[i] === sorted[j]) {
+            array.splice(i, 1); break here;
+          }
+        }
+      }
+    }
+    return sorted;
+  }
 
