58cb5ff8e887fb9b988f284e7afc188b4fd161f6
Roman <whisperdepth@gmail.com>
2020-04-03 17:08:42 +0300

    Add files via upload

---
+++ lesson6/hw82/index.js
@@ -0,0 +1,25 @@
+let sortAsc = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+};
+
+let sortDesc = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+};
