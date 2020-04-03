faf5b7bd02b1ccec78da9028f3accc554dc63c77
Roman <whisperdepth@gmail.com>
2020-04-03 20:59:57 +0300

    Add files via upload

---
+++ lesson6/hw9/index.js
@@ -0,0 +1,17 @@
+let clients = ["Ann", "John", "User"];
+let balances = [1400, 87, -6];
+
+function withdraw(clients, balances, client, amount) {
+  let index;
+  for (let i = 0; i < clients.length; i++) {
+    if (clients[i] === client) {
+      index = i;
+    }
+  }
+
+  if (balances[index] >= 0) {
+    return (balances[index] = balances[index] - amount);
+  } else {
+    return -1;
+  }
+}
