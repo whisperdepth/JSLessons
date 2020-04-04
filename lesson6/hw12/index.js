acbb710b7f9f259329c91381f32c5a49e97cba5d
Roman <whisperdepth@gmail.com>
2020-04-04 14:56:54 +0300

    Add files via upload

---
+++ lesson6/hw12/index.js
@@ -0,0 +1,13 @@
+function removeDuplicates(array){
+    if(!Array.isArray(array)){
+        return null;
+    }
+    for (let i = 0; i < array.length; i++){
+        for (let j = i + 1; j < array.length; j++){
+            if(array[i] == array[j]){
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
+}
