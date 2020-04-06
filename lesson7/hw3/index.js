6122691c4817797f2a93980ce58798a17458d3b7
Roman <whisperdepth@gmail.com>
2020-04-06 15:51:10 +0300

    Update index.js

--- lesson7/hw3/index.js
+++ lesson7/hw3/index.js
@@ -1,5 +1,9 @@
 const increaseEvenEl = (numbers, delta) => {
   if (!Array.isArray(numbers)) return null;
 
-  return numbers.map((num) => num + delta);
+  return numbers.map(num => {
+      if (num % 2 === 0 )
+         return num = num + delta;
+         return num;   
+  });
 };
