fa9b81a302e1e8da5aa61151a261f9d10161c608
Roman <whisperdepth@gmail.com>
2020-04-07 00:12:56 +0300

    Add files via upload

---
+++ lesson8/hw2/index.js
@@ -0,0 +1,20 @@
+const user = {
+  name: "Tom",
+  age: 17,
+};
+
+user.hobby = "football";
+
+const anotherKey = "married";
+
+user[anotherKey] = false;
+
+user["favorite music"] = "rock";
+
+user.adress = {};
+
+user.adress.country = "Ukraine";
+
+user.adress.building = 17;
+
+console.log(user);
