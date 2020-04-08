04035600f3378d5ba35a144a727110a335239b70
Roman <whisperdepth@gmail.com>
2020-04-09 01:24:24 +0300

    Update index.js

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,17 +1,18 @@
-
-
 const getRandomNumbers = (len, a, b) => {
     if (a > b) return null;
 
+    a = Math.ceil(a)
     b = Math.floor(b);
+
+    let rand = Math.floor(Math.random() * (b - a + 1) + a);
+    if(rand > b) return null;
+
     let arr = [];
-
-    for (let i = 0; i < len; i ++){
-        arr.push(Math.ceil(Math.random() * (b - a) + a));
+    for(let i = 0; i < len; i++){
+        arr.push(Math.floor(Math.random() * (b - a + 1) + a));
     }
-
-    return arr;
     
+    return arr;
 }
 
 
