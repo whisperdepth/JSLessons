4653272507c4327d3f434be8958d5867119f2b91
Roman <whisperdepth@gmail.com>
2020-04-08 15:46:52 +0300

    Update index.js

--- lesson9/hw1/index.js
+++ lesson9/hw1/index.js
@@ -1,5 +1,6 @@
 const getCustomersList = (customers) => {
-  let customersArray = Object.entries(customers);
+  let copy = { ...customers };
+  let customersArray = Object.entries(copy);
 
   let customersArrayChanged = customersArray.map((customer) => {
     customer[1].id = customersArray[customersArray.indexOf(customer)][0];
@@ -10,3 +11,4 @@ const getCustomersList = (customers) => {
     .map((customer) => customer[1])
     .sort((a, b) => a.age - b.age);
 };
+
