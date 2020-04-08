05722b5270d629548fc88445f9a3e598cdb44f7f
Roman <whisperdepth@gmail.com>
2020-04-08 22:37:07 +0300

    Update index.js

--- lesson10/hw1/index.js
+++ lesson10/hw1/index.js
@@ -1,5 +1,8 @@
 
 const getTotalPrice = numbers => {
     let sum = numbers.reduce((acc, num) => acc + num, 0);
-    return Math.floor(sum * 100)/100;;
+ 
+    let roundSum = Math.floor(sum * 100)/100;
+
+    return "$" + roundSum;
 }
