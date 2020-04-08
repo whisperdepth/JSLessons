f43466545522fbe05f9f39bf9358e2c2b60d0495
Roman <whisperdepth@gmail.com>
2020-04-08 15:36:46 +0300

    Add files via upload

---
+++ lesson9/hw1/index.js
@@ -0,0 +1,12 @@
+const getCustomersList = (customers) => {
+  let customersArray = Object.entries(customers);
+
+  let customersArrayChanged = customersArray.map((customer) => {
+    customer[1].id = customersArray[customersArray.indexOf(customer)][0];
+    return customer;
+  });
+
+  return customersArrayChanged
+    .map((customer) => customer[1])
+    .sort((a, b) => a.age - b.age);
+};
