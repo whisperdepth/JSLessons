6987a5b45d4ac8be6382344caa24f9410d77480d
Roman <whisperdepth@gmail.com>
2020-04-08 22:19:00 +0300

    Update index.js

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,4 +1,4 @@
 const getMaxAbsoluteNumber = (numbers) =>{
   if(!Array.isArray(numbers) || numbers.length == 0) return null;
-  Math.max(...numbers.map((num) => Math.abs(num)))
+  return Math.max(...numbers.map((num) => Math.abs(num)))
 };
