87e2e628dfa189eb1f8f685ec279c9520743d156
Roman <whisperdepth@gmail.com>
2020-04-09 01:41:41 +0300

    Add files via upload

---
+++ lesson10/hw3/index.js
@@ -0,0 +1,8 @@
+const multiRound = (num, n) => [
+    Math.floor(num *  Math.pow(10, n)) /  Math.pow(10, n), 
+    Math.round(num *  Math.pow(10, n)) /  Math.pow(10, n), 
+    Math.ceil(num *  Math.pow(10, n)) /  Math.pow(10, n),
+    Math.trunc(num *  Math.pow(10, n)) /  Math.pow(10, n), 
+    +num.toFixed(n)
+]
+
