7c22d57d5ff0865d32a2aac69fbbc8250c35ea9e
Roman <whisperdepth@gmail.com>
2020-04-09 14:18:55 +0300

    Add files via upload

---
+++ lesson11/task1/index.js
@@ -0,0 +1,15 @@
+const splitText = (text, len) => {
+    if(typeof text != "string") return null;
+    let text2 = '';
+    let start = 0;
+    
+    while(true){
+        let chunk = text.substr(start, len);
+        if(chunk.length === 0) break;
+
+        text2 = text2 + (chunk[0].toUpperCase() + (chunk.slice(1) +"\n"));
+        start += len;
+
+    }
+    return text2;
+}
