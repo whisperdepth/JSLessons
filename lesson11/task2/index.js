f3f3fe2628a7400712bd8aeac4f47bc342d1f4cd
Roman <whisperdepth@gmail.com>
2020-04-09 16:49:33 +0300

    Update index.js

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -1,7 +1,7 @@
 const sortContacts = (contacts, boolean) => {
   if (!Array.isArray(contacts)) return null;
   if (boolean == false)
-  return contacts.sort((a, b) => b.name.localCompare(a.name));
+  return contacts.sort((a, b) => b.name.localeCompare(a.name));
 
-  return contacts.sort((a, b) => a.name.localCompare(b.name));
+  return contacts.sort((a, b) => a.name.localeCompare(b.name));
 };
