6a0e04dbcc3644ea06dd9e023ca25fa96134d502
Roman <whisperdepth@gmail.com>
2020-04-09 17:31:29 +0300

    Update index.js

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -1,11 +1,5 @@
-let str = "asd.ddff.dfff.d";
-let substr = 'dff'
-
-
 let countOccurrences = (str, subsrt) => {
     if(subsrt == "") return null;
 
     return str.split(subsrt).length - 1
 }
-
-console.log(countOccurrences(str, substr));
