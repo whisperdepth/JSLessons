7cda30967747bec9e222b8878c8b5639c48a84bc
Roman <whisperdepth@gmail.com>
2020-04-09 17:02:52 +0300

    Add files via upload

---
+++ lesson11/hw1/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let text = "gegergggdgdgdgdgrergggdyhjyjyj";
+
+
+const splitText = (text, len) => {
+    if(typeof text != "string") return null;
+    if (len === undefined) len = 10;
+
+    let strArr = [];
+    let start = 0;
+    
+    while(true){
+        let chunk = text.substr(start, len);
+        if(chunk.length === 0) break;
+        if(chunk.length < len){
+            while(chunk.length < len){
+                chunk += ".";
+            }
+        }
+
+        strArr.push(chunk);
+        start += len;
+
+    }
+    return strArr;
+}
+
+
+console.log(splitText(text, 4));
