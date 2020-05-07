bd5b584d30e4f43df6fe9985784658a0a1be31ec
Roman <whisperdepth@gmail.com>
2020-05-07 12:36:58 +0300

    Update index.js

--- lesson9/hw2/index.js
+++ lesson9/hw2/index.js
@@ -1,11 +1,7 @@
 
 
-const getPeople = (rooms) => {
-
-  let roomsArrFlat = Object.entries(rooms).map((room) => room[1]).flat();
-
-  let roomsArrFiltered = roomsArrFlat.map((room) => Object.entries(room)).map((room) => room[0]);
-
-  return roomsArrFiltered.map((room) => room[1]);
-};
+const getPeople = (rooms) =>
+  Object.values(rooms)
+    .flat()
+    .map((room) => room.name);
 
